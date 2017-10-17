3e76cf705e30e42573f62f863393b1dfc0876b09
Dougy Ouyang <ouyangdougy@163.com>
2017-10-16 17:30:24 -0700

    Create Bookshelf.cpp

---
+++ Bookshelf.cpp
@@ -0,0 +1,58 @@
+/*
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org/>
+
+*/
+
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+int cow[20200];
+
+int main()
+{
+    int n, s;
+    int sum=0, cows=0;
+    int i;
+    
+    cin >> n >> s;
+    for(i=0;i<n;i++){
+        cin >> cow[i];
+    }
+    
+    sort(cow, cow+n);
+    for(i=n-1;i>=0;i--){
+        sum+=cow[i], cows++;
+        if(sum>=s)
+            break;
+    }
+    
+    cout << cows << endl;
+    
+    return 0;
+}
